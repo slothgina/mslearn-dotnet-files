b38b779c28d3bacb2e6a2e8f1f97c344da89a27a
slothgina <gina.slothstudio@zohomail.com>
2026-05-18 13:53:23 -0400

    Exercise - Create files and directories Create the SalesTotals directory Write the totals.txt file

--- Program.cs
+++ Program.cs
@@ -3,20 +3,82 @@ using System.Collections.Generic;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);     
 var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
 {
-    Console.WriteLine(file);
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);   // Add this line of code
+
+//var salesFiles = FindFiles(storesDirectory);
 //var salesFiles = FindFiles("stores");
+IEnumerable<string> FindFiles(string folderName)
+{
+    string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+    Console.WriteLine(Path.Combine("stores","201")); // outputs: stores/201
+    Console.WriteLine(Path.GetExtension("sales.json")); // outputs: .json
+}
 
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);*/
+
+
+//Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir"));
+//bool doesDirectoryExist = Directory.Exists(filePath);
+//File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt"), "Hello World!");
+/*var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+var salesFiles = FindFiles("stores");
 foreach (var file in salesFiles)
 {
     Console.WriteLine(file);
 }
-
+//foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+var salesFiles = FindFiles("stores");
 IEnumerable<string> FindFiles(string folderName)
 {
     string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
@@ -29,26 +91,20 @@ string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparat
 FileInfo info = new FileInfo(fileName);
 
 Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
-
-
 // returns:
 // stores\201 on Windows
 //
-// stores/201 on macOS
-    List<string> salesFiles = new List<string>();
+// stores/201 on macOS*/
+    //List<string> salesFiles = new List<string>();
 
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+    //var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
-    foreach (var file in foundFiles)
-    {
-        var extension = Path.GetExtension(file);
-        //if (file.EndsWith("sales.json"))
+   // foreach (var file in foundFiles)
+    //{
         //var extension = Path.GetExtension(file);
-        if (extension == ".json")
-        {
-            salesFiles.Add(file);
-        }
-    }
+       // if (file.EndsWith("sales.json"))
+    
+        //if (extension == ".json")
+        //{
+           // salesFiles.Add(file);
 
-    return salesFiles;
-}
