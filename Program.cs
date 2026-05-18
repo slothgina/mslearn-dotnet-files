6cf71d50233e2176de9cf94b3b68e4c009347131
slothgina <gina.slothstudio@zohomail.com>
2026-05-18 15:04:41 -0400

    Exercise - Read and write to files Add Newtonsoft.Json to the project Prepare for sales data Create a method that calculates sales totals Call the CalculateSalesTotals method Write the total to the totals.txt file Run the program idk what tf

--- Program.cs
+++ Program.cs
@@ -1,44 +1,68 @@
-﻿using Newtonsoft.Json;
-
-IEnumerable<string> FindFiles(string folderName)
+﻿internal class Program
 {
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
-    return foundFiles;
-}
-foreach (var file in FindFiles(storesDirectory))
+    private static void Main(string[] args)
     {
-        var extension = Path.GetExtension(file);
-        if (extension == ".json")
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+        var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+        Directory.CreateDirectory(salesTotalDir);
+
+        var salesFiles = FindFiles(storesDirectory);
+
+        var salesTotal = CalculateSalesTotal(salesFiles);
+
+        File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+        IEnumerable<string> FindFiles(string folderName)
         {
-            salesFiles.Add(file);
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+                if (extension == ".json")
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+        }
+
+        double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
+
+            // Loop over each file path in salesFiles
+            foreach (var file in salesFiles)
+            {
+                // Read the contents of the file
+                string salesJson = File.ReadAllText(file);
+
+                // Parse the contents as JSON
+                SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+                // Add the amount found in the Total field to the salesTotal variable
+                salesTotal += data?.Total ?? 0;
+            }
+
+            return salesTotal;
         }
     }
-    return salesFiles;
-
-//using System.IO;
-//using System.Collections.Generic;
-
-var currentDirectory = Directory.GetCurrentDirectory();
-var storesDirectory = Path.Combine(currentDirectory, "stores");
-
-var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);     
-var salesFiles = FindFiles(storesDirectory);
-
-
-var data = JsonConvert.DeserializeObject<SalesTotal>((string?)salesJson);
-File.AppendAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", $"{data.Total}{Environment.NewLine}");
-
-File.WriteAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", data.Total.ToString());
-var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
-var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
-
-Console.WriteLine(salesData.Total);
-
-class SalesTotal
-{
-  public double Total { get; set; }
 }
+
+internal class JsonConvert
+{
+    internal static T DeserializeObject<T>(string salesJson)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+record SalesData (double Total);
 
 
 
