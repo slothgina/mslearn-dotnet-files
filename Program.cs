e20f02af665b15cfb1dd63e7a7f967939ab67cb4
slothgina <gina.slothstudio@zohomail.com>
2026-05-18 13:01:02 -0400

    There are many more useful properties and utility methods on the Path, DirectoryInfo, and FileInfo classes, but these core concepts are the ones that you're likely to use most often. In the next exercise, you'll compose paths and identify .json files.

--- Program.cs
+++ Program.cs
@@ -19,7 +19,22 @@ foreach (var file in salesFiles)
 
 IEnumerable<string> FindFiles(string folderName)
 {
+    string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+    Console.WriteLine(Path.Combine("stores","201")); // outputs: stores/201
+    Console.WriteLine(Path.GetExtension("sales.json")); // outputs: .json
 
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+FileInfo info = new FileInfo(fileName);
+
+Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+
+
+// returns:
+// stores\201 on Windows
+//
+// stores/201 on macOS
     List<string> salesFiles = new List<string>();
 
     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
