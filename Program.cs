d18879f7f83024aa1c8466c3f60ba75ca1786621
slothgina <gina.slothstudio@zohomail.com>
2026-05-19 15:23:08 -0400

    i dont even know if this is right

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json; 
+
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);
+Directory.CreateDirectory(salesTotalDir);   
+
 var salesFiles = FindFiles(storesDirectory);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesTotal = CalculateSalesTotal(salesFiles);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -32,122 +32,24 @@ IEnumerable<string> FindFiles(string folderName)
     return salesFiles;
 }
 
-Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir"));
-//bool doesDirectoryExist = Directory.Exists(filePath);
-File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt"), "Hello World!");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*Console.WriteLine(Directory.GetCurrentDirectory());
-string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
-
-// returns:
-// stores\201 on Windows
-//
-// stores/201 on macOS
-Console.WriteLine(Path.Combine("stores","201")); // outputs: stores/201
-Console.WriteLine(Path.GetExtension("sales.json")); // outputs: .json
-
-string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
-
-FileInfo info = new FileInfo(fileName);
-
-Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more*/
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+    
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+    
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+    
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+    
+    return salesTotal;
+}
+
+record SalesData (double Total);
